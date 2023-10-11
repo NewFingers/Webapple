e29955dc07d5eb95c3ba1bc70599714419da37d4
Felix Schwips <felix4schwips@gmail.com>
2023-10-11 18:49:31 +0200

    JS: API Call, Input to JSON obj

--- Backend.js
+++ Backend.js
@@ -1,2 +1,23 @@
-const search = document.getElementById("search");
-const listcontainer = document.getElementById("container-list");
+const search = document.querySelector('form');
+search.addEventListener('submit', (sub) => {
+    sub.preventDefault();
+    const fd = new FormData(search);
+    const obj = Object.fromEntries(fd);
+    console.log(obj);
+    const json = JSON.stringify(obj);
+    
+    console.log(json);
+})
+
+fetch ('https://date.nager.at/api/v3/PublicHolidays/2023/DE')
+.then(res => {
+  return res.json(); 
+})
+.then(daten => {
+   daten.forEach(tag => {
+       const listitem = `<li> ${tag.date} ${tag.localName} </li>`
+       document.querySelector('ul').insertAdjacentHTML("beforeend",listitem);
+   }); 
+}); 
+
+
