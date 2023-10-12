a8e735628fecb3646473c119cadfbfd2d4daf11a
Felix Schwips <felix4schwips@gmail.com>
2023-10-12 12:08:11 +0200

    JS: Add Task, Neue Datenbehandlung aus Abfrage

--- Backend.js
+++ Backend.js
@@ -1,4 +1,4 @@
-const search = document.querySelector('form');
+/* const search = document.querySelector('form');
 search.addEventListener('submit', (sub) => {
     sub.preventDefault();
     const fd = new FormData(search);
@@ -7,7 +7,16 @@ search.addEventListener('submit', (sub) => {
     const json = JSON.stringify(obj);
     
     console.log(json);
+
+    
+
 })
+*/
+
+
+
+const search = document.getElementById("search");
+const conlist = document.getElementById("container-list");
 
 fetch ('https://date.nager.at/api/v3/PublicHolidays/2023/DE')
 .then(res => {
@@ -15,9 +24,40 @@ fetch ('https://date.nager.at/api/v3/PublicHolidays/2023/DE')
 })
 .then(daten => {
    daten.forEach(tag => {
-       const listitem = `<li> ${tag.date} ${tag.localName} </li>`
+       const listitem = `${tag.date} ${tag.localName}`;
+       let li = document.createElement("li");
+        li.innerHTML = listitem;
+        conlist.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+   }); 
+});
+
+function addTask() {
+    if (search.value === "") {
+        alert ("Das Eingabefeld ist leer")
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = search.value;
+        conlist.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    search.value = " ";
+
+}
+/*
+fetch ('https://date.nager.at/api/v3/PublicHolidays/2023/DE')
+.then(res => {
+  return res.json(); 
+})
+.then(daten => {
+   daten.forEach(tag => {
+       const listitem = `<li> ${tag.date} ${tag.localName} </li>`;
        document.querySelector('ul').insertAdjacentHTML("beforeend",listitem);
    }); 
 }); 
 
-
+*/
